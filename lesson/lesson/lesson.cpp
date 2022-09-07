9380a278864fbd8efcfe55bc4a5c06d33da6584c
Pear Poor <salaev_08@mail.ru>
2022-09-07 14:14:42 +0300

    commit

--- lesson/lesson/lesson.cpp
+++ lesson/lesson/lesson.cpp
@@ -1,11 +1,49 @@
-#include "iostream"
+#include <iostream>
+#include <string>
 
 
 using namespace std;
 
 
+class Cord{
+	int x, y;
+
+public:
+
+	Cord(int _x, int _y) {
+		this->x = _x;
+		this->y = _y;
+	};
+
+	Cord(const Cord &c) {
+		this->x = c.x;
+		this->y = c.y;
+	};
+
+
+	const Cord operator+(const int& num) {
+		this->x += y;
+		this->y += x;
+
+		return *this;
+	};
+
+	void print() {
+		cout << "x = " << x 
+			<< endl 
+			<< "y = " << y 
+			<< endl;
+	}
+};
+
+
 int main() {
-	cout << "Hello world!" << endl;
+	Cord cordinates(1, 2);
+	Cord coord;
+	cordinates.print();
+	coord.print();
+
+
 }
 
 
